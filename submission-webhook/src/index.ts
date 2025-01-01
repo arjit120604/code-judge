947c4881b679968fdee840ccf277d3c1749303b6
arjit120604 <130384377+arjit120604@users.noreply.github.com>
2025-01-01 23:23:42 +0530

    restructure code

---
+++ submission-webhook/src/index.ts
@@ -0,0 +1,11 @@
+import expresss from 'express';
+import prisma from '../src/lib/prisma';
+
+const app = expresss();
+
+app.use(expresss.json());
+
+app.post('/submission-webhook', async (req, res) => {
+
+    if (!req.)
+});
